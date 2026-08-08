362d971400b363ea492c960ac60128436278f0d7
K3ROIN <kemanunramungtong@gmail.com>
2026-08-08 14:32:46 +0700

    Step 7: Button Widget

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -5,13 +5,61 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [Image.asset('assets/images/jane-doe-zzz.png', width: 200, height: 200), 
-      SizedBox(height: 20),
-      Image.network('https://media.tenor.com/P51U5i_F1pYAAAAM/zzz-zenless-zone-zero.gif', width: 200, height: 200),
-      ],
-    );
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton (
+            style: TextButton.styleFrom(backgroundColor: Colors.orangeAccent),
+            onPressed: () {
+              print('Pressed TextButton');
+            },
+            child: Text(
+              'Click Me',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            )
+          ),
+          SizedBox(height: 20),
+          FilledButton(
+            style: FilledButton.styleFrom(
+              backgroundColor: Colors.greenAccent, 
+              foregroundColor: Colors.white),
+            onPressed: () {
+              print('Pressed FilledButton');
+            },
+            child: Text(
+              'Click Me',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 20),
+          OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              side: BorderSide(color: Colors.greenAccent, width: 2),
+            ),
+            onPressed: () {
+              print('Pressed OutlinedButton');
+            },
+            child: Text(
+              'Click Me',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.pinkAccent, 
+              foregroundColor: Colors.white),
+            onPressed: () {
+              print('Pressed ElevatedButton');
+            },
+            child: Text(
+              'Click Me',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    ); 
   }
 }
