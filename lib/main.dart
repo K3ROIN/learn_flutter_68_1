3b1b7fd1329f6218995189ab78b45888d1d28e8e
K3ROIN <kemanunramungtong@gmail.com>
2026-08-22 13:45:25 +0700

    Step 17: Route Nav

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 // import 'package:learn_flutter_68_1/screen/home.dart';
+import 'package:learn_flutter_68_1/screen/item.dart';
+// import 'package:learn_flutter_68_1/screen/addForm.dart';
 // import 'package:learn_flutter_68_1/screen/item.dart';
-import 'package:learn_flutter_68_1/screen/addForm.dart';
 
 void main() {
   // runApp(const MyApp());
@@ -23,5 +24,25 @@ void main() {
 //   );
 // }
 
-runApp(AddForm());
+// runApp(AddForm());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      title: "Add Person",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Add Person"),
+          backgroundColor: Colors.greenAccent,
+          centerTitle: true,
+        ),
+        body: Item(),
+      )
+    );
+  }
 }
